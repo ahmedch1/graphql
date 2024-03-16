006c9d023c0889f4bc1642b2f3d93a14558541bf
Ahmed Chouihi <ahmedchouihi@gmail.com>
2024-03-16 17:56:54 +0100

    Resolver Functions

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
 import {ApolloServer} from "@apollo/server";
 import {startStandaloneServer} from "@apollo/server/standalone";
+import {typeDefs} from "./schema.js";
+import db from './_db.js'
+
+
+const resolvers={
+    Query:{
+        games(){
+            return db.games
+        },
+        authors(){
+            return db.authors
+        },
+        reviews(){
+            return db.reviews
+        }
+    }
+}
+
+/*
+games{
+title
+}
+*/
 
 //server setup
 const server=new ApolloServer({
-// typeDefs
+// types
+    typeDefs,
+    resolvers
+
 // resolvers
 });
 
